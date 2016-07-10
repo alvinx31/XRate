9b1cb0cd0628ec1ca2d601e725853f4d83d8e9a5
Moon <moonlade@gmail.com>
2016-07-10 08:16:39 +0200

    refactor functions name.

--- compute-hist-rate.py
+++ compute-hist-rate.py
@@ -8,10 +8,10 @@ hash_file = "rate.txt"
 
 # Get historical rates for any day since 1999.
 # http://api.fixer.io/2000-01-03?base=USD
-def GenerateUrl(day):
+def generate_url(day):
 	return "http://api.fixer.io/{:%Y-%m-%d}?base=USD".format(day)
 
-def CalcAvgStd(xrate_map, n):
+def calc_avg_std(xrate_map, n):
 	avg = float(sum(xrate_map)) / n
 	sq = sum((x-avg)**2 for x in xrate_map)
 	# Divide by (n-1), from Bessel's correction.
@@ -21,7 +21,7 @@ def CalcAvgStd(xrate_map, n):
 	midv = (maxv + minv) * 0.5
 	print "Latest {0:} day(s), Mid: {3:.3f}, Avg: {1:.3f}, StdDev: {2:.3f}".format(n, avg, std, midv)
 
-def LoadHash():	
+def load_hash():	
 	xrate_hash = {}
 	try:
 		with open(hash_file, "r") as f:
@@ -32,38 +32,38 @@ def LoadHash():
 		pass
 	return xrate_hash
 
-def DumpHash(xrate_hash):
+def dump_hash(xrate_hash):
 	with open(hash_file, "w") as f:
 		for k, v in xrate_hash.iteritems():
 			f.write("{},{}\n".format(k,v))			
 
-def HashKey(day):
+def get_hash_key(day):
 	return "{:%Y-%m-%d}".format(day)
 
-def GetDayRate(day, xrate_hash):
-	k = HashKey(day)
+def get_day_rate(day, xrate_hash):
+	k = get_hash_key(day)
 	if xrate_hash.has_key(k):
 		return xrate_hash[k]		
-	day_rate = json.loads(urllib2.urlopen(GenerateUrl(day)).read())
+	day_rate = json.loads(urllib2.urlopen(generate_url(day)).read())
 	xrate_hash[k] = day_rate['rates']['CHF']
 	return day_rate['rates']['CHF']
 
-def Main():
+def main():
 	today = datetime.date.today()
-	xrate_hash = LoadHash()
+	xrate_hash = load_hash()
 	xrate_map = []
-	
+
 	for n in range(k_total_days):
 		day = today - datetime.timedelta(n)
-		chf = GetDayRate(day, xrate_hash)
+		chf = get_day_rate(day, xrate_hash)
 		xrate_map.append(chf);
 
 	for day in days_to_stats:
 		if day > k_total_days:
 			break
-		CalcAvgStd(xrate_map[:day], day)
+		calc_avg_std(xrate_map[:day], day)
 	
-	DumpHash(xrate_hash)
+	dump_hash(xrate_hash)
 
 if __name__ == "__main__":
-	Main()
+	main()
